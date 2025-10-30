5524a6f05a1eba10f00067f244c480686b199b86
Ryoichi Sakamaki <gerggms14@gmail.com>
2025-10-30 12:42:44 +0900

    6th work2

--- test.bash
+++ test.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# SPDX-FileCopyrightText: 2025 Ryoichi Sakamaki 　　　　　
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
 	echo ${1}行目が違うよ
@@ -7,8 +8,11 @@ ng () {
 }
 
 res=0
-a=坂巻
-[ "$a" = 酒巻 ] || ng "$LINENO"
-[ "$a" = 坂巻 ] || ng "$LINENO"
+
+out=$(seq 5 | ./good123)
+
+
+[ "${out}" = 15 ] || ng "$LINENO"
+[ "${res}" = 0 ] && echo OK
 
 exit $res
